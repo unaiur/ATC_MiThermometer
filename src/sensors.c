e9a3fbc6e2740cb77dba9ad42ff697890aa69bd3
Unai Uribarri <unaiur@gmail.com>
2022-04-10 00:47:03 +0200

    Rework and simplify temperature sensor code

--- src/sensors.c
+++ src/sensors.c
@@ -38,9 +38,9 @@
 #define CRC_POLYNOMIAL  0x131 // P(x) = x^8 + x^5 + x^4 + 1 = 100110001
 
 static RAM uint8_t sensor_i2c_addr;
-static RAM bool sensor_busy;
+static RAM bool sensor_idle;
 static RAM uint32_t next_awake;
-static RAM uint32_t last_read;
+static RAM uint32_t next_read;
 
 static inline uint32_t get_read_interval_us()
 {
@@ -86,13 +86,13 @@ static void sensor_reset(void)
 
 void sensor_turn_off(void)
 {
-	if (sensor_busy && is_shtc3()) {
-		sensor_busy = false;
+	if (!sensor_idle && is_shtc3()) {
 		if ((reg_clk_en0 & FLD_CLK0_I2C_EN) == 0) {
 				init_i2c();
 		}
 		sensor_reset();
 	}
+	sensor_idle = true;
 }
 
 bool sensor_is_shtc3()
@@ -108,94 +108,101 @@ bool sensor_is_shtc3()
 
 void sensor_init()
 {
-	last_read = next_awake = uclock_awake_after(0);
+	next_read = next_awake = uclock_awake_after(0);
 	if (is_shtc3()) {
 		send_sensor_word(SHTC3_WAKEUP); //	Wake-up command of the sensor
 		sleep_us(SHTC3_WAKEUP_us);	// 240 us
 	}
 	sensor_reset();
+	sensor_idle = true;
 }
 
-static _attribute_ram_code_ __attribute__((optimize("-Os"))) int read_sensor_cb(void) {
-	uint16_t _temp;
-	uint16_t _humi;
-	uint8_t data, crc; // calculated checksum
-	int i;
+static _attribute_ram_code_ u8 update_crc8(u8 data, u8 crc)
+{
+	crc ^= data;
+	for (int i = 8; i > 0; i--) {
+		if(crc & 0x80)
+			crc = (crc << 1) ^ (CRC_POLYNOMIAL & 0xff);
+		else
+			crc = (crc << 1);
+	}
+	return crc;
+}
+
+static _attribute_ram_code_ bool read_word(uint16_t *value)
+{
+	reg_i2c_ctrl = FLD_I2C_CMD_DI | FLD_I2C_CMD_READ_ID;
+	while(reg_i2c_status & FLD_I2C_CMD_BUSY);
+	u8 firstByte = reg_i2c_di;
+
+	reg_i2c_ctrl = FLD_I2C_CMD_DI | FLD_I2C_CMD_READ_ID;
+	u8 crc = update_crc8(firstByte, 0xff);
+	while(reg_i2c_status & FLD_I2C_CMD_BUSY);
+	u8 secondByte = reg_i2c_di;
+
+	reg_i2c_ctrl = FLD_I2C_CMD_DI | FLD_I2C_CMD_READ_ID;
+	crc = update_crc8(secondByte, crc);
+	while(reg_i2c_status & FLD_I2C_CMD_BUSY);
+	u8 received_crc = reg_i2c_di;
+
+	if (crc == received_crc) {
+		*value = (firstByte << 8) | secondByte;
+		return true;
+	}
+	return false;
+}
+
+static _attribute_ram_code_ bool read_sensor_cb(void)
+{
 	reg_i2c_id = sensor_i2c_addr | FLD_I2C_WRITE_READ_BIT;
-	i = 512;
-	do {
+
+	for (int retries = 0; retries < 5; ++retries) {
 		reg_i2c_ctrl = FLD_I2C_CMD_ID | FLD_I2C_CMD_START;
 		while(reg_i2c_status & FLD_I2C_CMD_BUSY);
-		if(reg_i2c_status & FLD_I2C_NAK) {
-			reg_i2c_ctrl = FLD_I2C_CMD_STOP;
-			while(reg_i2c_status & FLD_I2C_CMD_BUSY);
-		} else { // ACK ok
-			reg_i2c_ctrl = FLD_I2C_CMD_DI | FLD_I2C_CMD_READ_ID;
-			while(reg_i2c_status & FLD_I2C_CMD_BUSY);
-			data = reg_i2c_di;
-			reg_i2c_ctrl = FLD_I2C_CMD_DI | FLD_I2C_CMD_READ_ID;
-			_temp = data << 8;
-			crc = data ^ 0xff;
-			for(i = 8; i > 0; i--) {
-				if(crc & 0x80)
-					crc = (crc << 1) ^ (CRC_POLYNOMIAL & 0xff);
-				else
-					crc = (crc << 1);
-			}
-			while(reg_i2c_status & FLD_I2C_CMD_BUSY);
-			data = reg_i2c_di;
-			reg_i2c_ctrl = FLD_I2C_CMD_DI | FLD_I2C_CMD_READ_ID;
-			_temp |= data;
-			crc ^= data;
-			for(i = 8; i > 0; i--) {
-				if(crc & 0x80)
-					crc = (crc << 1) ^ (CRC_POLYNOMIAL & 0xff);
-				else
-					crc = (crc << 1);
-			}
-			while(reg_i2c_status & FLD_I2C_CMD_BUSY);
-			data = reg_i2c_di;
-			reg_i2c_ctrl = FLD_I2C_CMD_DI | FLD_I2C_CMD_READ_ID;
-			while(reg_i2c_status & FLD_I2C_CMD_BUSY);
-			_humi = reg_i2c_di << 8;
-			reg_i2c_ctrl = FLD_I2C_CMD_DI | FLD_I2C_CMD_READ_ID | FLD_I2C_CMD_ACK;
-			while(reg_i2c_status & FLD_I2C_CMD_BUSY);
-			_humi |= reg_i2c_di;
-			reg_i2c_ctrl = FLD_I2C_CMD_STOP;
-			while(reg_i2c_status & FLD_I2C_CMD_BUSY);
-			if(crc == data && _temp != 0xffff) {
-				measured_data.temp = ((int32_t)(17500*_temp) >> 16) - 4500 + cfg.temp_offset * 10; // x 0.01 C
-				if (is_shtc3())
-					measured_data.humi = ((uint32_t)(10000*_humi) >> 16) + cfg.humi_offset * 10; // x 0.01 %
-				 else
-					measured_data.humi = ((uint32_t)(12500*_humi) >> 16) - 600 + cfg.humi_offset * 10; // x 0.01 %
-				if(measured_data.humi < 0) measured_data.humi = 0;
-				else if(measured_data.humi > 9999) measured_data.humi = 9999;
-				measured_data.count++;
-				if (is_shtc3())
-					send_sensor_word(SHTC3_GO_SLEEP); // Sleep command of the sensor
-				return 1;
-			}
+
+		uint16_t _temp;
+		uint16_t _humi;
+		bool valid = !(reg_i2c_status & FLD_I2C_NAK) && read_word(&_temp) && _temp != 0xffff && read_word(&_humi);
+
+		reg_i2c_ctrl = FLD_I2C_CMD_STOP;
+		while(reg_i2c_status & FLD_I2C_CMD_BUSY);
+
+		if (valid) {
+			measured_data.temp = ((int32_t)(17500*_temp) >> 16) - 4500 + cfg.temp_offset * 10; // x 0.01 C
+			if (is_shtc3())
+				measured_data.humi = ((uint32_t)(10000*_humi) >> 16) + cfg.humi_offset * 10; // x 0.01 %
+			else
+				measured_data.humi = ((uint32_t)(12500*_humi) >> 16) - 600 + cfg.humi_offset * 10; // x 0.01 %
+			if (measured_data.humi < 0)
+				measured_data.humi = 0;
+			else if(measured_data.humi > 9999)
+				measured_data.humi = 9999;
+			measured_data.count++;
+			if (is_shtc3())
+				send_sensor_word(SHTC3_GO_SLEEP); // Sleep command of the sensor
+			return true;
 		}
-	} while(i--);
+	}
 	sensor_reset();
-	return 0;
+	return false;
 }
 
-static _attribute_ram_code_ void start_measure_sensor_deep_sleep(void) {
+static _attribute_ram_code_ void start_measure_sensor_deep_sleep(void)
+{
 	if (is_shtc3()) {
 		send_sensor_word(SHTC3_WAKEUP); //	Wake-up command of the sensor
 		sleep_us(SHTC3_WAKEUP_us - 5);	// 240 us
 		send_sensor_word(SHTC3_MEASURE);
-	} else if (is_sht4x())
+	} else if (is_sht4x()) {
 		send_sensor_byte(SHT4x_MEASURE_HI);
+	}
 	gpio_setup_up_down_resistor(I2C_SCL, PM_PIN_PULLUP_1M);
 	gpio_setup_up_down_resistor(I2C_SDA, PM_PIN_PULLUP_1M);
 }
 
 _attribute_ram_code_ bool sensor_is_idle()
 {
-	return !sensor_busy;
+	return sensor_idle;
 }
 
 _attribute_ram_code_ bool sensor_read()
@@ -207,11 +214,13 @@ _attribute_ram_code_ bool sensor_read()
 		init_i2c();
 	}
 	bool result = false;
-	if (!sensor_busy) {
+	if (sensor_idle) {
+		sensor_idle = false;
 		start_measure_sensor_deep_sleep();
 		next_awake = uclock_awake_after(SENSOR_MEASURING_TIMEOUT_ms * 1000);
 		check_battery();
 	} else {
+		sensor_idle = true;
 #if USE_TRIGGER_OUT && defined(GPIO_RDS)
 		rds_input_on();
 #endif
@@ -228,8 +237,7 @@ _attribute_ram_code_ bool sensor_read()
 #if USE_TRIGGER_OUT && defined(GPIO_RDS)
 		rds_input_off();
 #endif
-		next_awake = uclock_awake_at(last_read += get_read_interval_us());
+		next_awake = uclock_awake_at(next_read += get_read_interval_us());
 	}
-	sensor_busy = !sensor_busy;
 	return result;
 }
